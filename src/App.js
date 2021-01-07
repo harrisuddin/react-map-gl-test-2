e691c5f251c5630532f2230e316fedd0e3b66e7d
harrisuddin <harris7001@gmail.com>
2021-01-07 12:54:34 +0000

    made new container component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Map from "./Map";
 import Camera from "./Camera";
+import Container from "./components/Container";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/container">
+          <Container />
+        </Route>
         <Route path="/map">
           <Map />
         </Route>
