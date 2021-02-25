f78a746f9083c72d62ea2b07a8f137d870240c8f
Uddin, Harris <harris7001@gmail.com>
2021-02-25 18:14:30 +0000

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import Map from "./Map";
-import Camera from "./Camera";
-import Container from "./components/Container";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/container">
-          <Container />
-        </Route>
-        <Route path="/map">
-          <Map />
-        </Route>
         <Route path="/">
-          <Camera />
+          <Map />
         </Route>
       </Switch>
     </Router>
